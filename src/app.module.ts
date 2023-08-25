f1d3126b8c3a6e49abdae4be32e98236cfa8703a
Long <khanhlong1315@gmail.com>
2023-08-25 15:32:18 +0700

    Feat: add Joi and Validation options

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { database_config } from './configs/configuration.config';
+import Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision', 'staging')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: {
+        abortEarly: false,
+      },
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
       load: [database_config],
