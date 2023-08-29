68c57c1878429d4cf26a59935c1a9aaab7e3476d
Long <khanhlong1315@gmail.com>
2023-08-29 14:45:55 +0700

    feat: setup path aliases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { database_config } from './configs/configuration.config';
+import { database_config } from '@configs/configuration.config';
 import Joi from 'joi';
 
 @Module({
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
-import { DatabaseConfig } from './configs/configuration.config';
+import { DatabaseConfig } from '@configs/configuration.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
